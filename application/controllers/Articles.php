85fc25455983132446141821bdad2297c5ae9523
alvin <alvin.odins@gmail.com>
2017-06-24 13:46:09 -0700

    add article view function | add view page

--- application/controllers/Articles.php
+++ application/controllers/Articles.php
@@ -12,5 +12,20 @@
       $this->load->view('articles/index', $data);
       $this->load->view('templates/footer');
     }
+
+    public function view($slug = NULL){
+      $data['article'] = $this->articles_model->get_articles($slug);
+
+      if (empty($data['articles'])) {
+        show_404();
+      }
+
+      $data['title'] = $data['article']['title'];
+
+      $this->load->view('templates/header');
+      $this->load->view('templates/nav');
+      $this->load->view('articles/view', $data);
+      $this->load->view('templates/footer');
+    }
   }
  ?>
