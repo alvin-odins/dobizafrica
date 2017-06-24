e65b0dbbaaa8c6cd47bc00289ac716c9e776d26a
alvin <alvin.odins@gmail.com>
2017-06-24 13:42:31 -0700

    edit articles view page

--- application/views/articles/index.php
+++ application/views/articles/index.php
@@ -6,6 +6,8 @@
     <h3><?php echo $article['title']; ?></h3>
     <small class="post-date">Posted on: <?php echo $article['created_at']; ?></small><br>
     <?php echo $article['body']; ?>
+    <br><br>
+    <p><a class="btn btn-primary" href="<?php echo site_url('/articles/'.$article['slug']); ?>">Read More</a></p>
 
   <?php endforeach; ?>
 </div>
