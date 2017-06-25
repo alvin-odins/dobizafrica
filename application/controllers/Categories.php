3fe5440b93156ff2d2275821980e0aa22437345a
alvin <alvin.odins@gmail.com>
2017-06-25 03:58:51 -0700

    create categories dir and controller

---
+++ application/controllers/Categories.php
@@ -0,0 +1,20 @@
+<?php 
+	class Categories extends CI_Controller{
+
+		public function create(){
+			$data['title'] = 'Create Article Category';
+
+			$this->form_validation->set_rules('name', 'Name', 'required');
+
+			if ($this->form_validation->run() === FALSE) {
+				$this->load->view('templates/header');
+				$this->load->view('templates/nav');
+				$this->load->view('templates/create', $data);
+				$this->load->view('templates/footer');
+			} else {
+				@this->category_model->create_category();
+				redirect('categories');
+			}
+		}
+	}
+?>
---
+++ application/views/categories/create.php
@@ -0,0 +1,4 @@
+<h2><?= $title; ?></h2>
+
+<?php echo validation_errors(); ?>
+
