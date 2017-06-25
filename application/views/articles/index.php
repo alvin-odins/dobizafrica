35e97854208d31c7998299eb8d14da4e17101c61
alvin <alvin.odins@gmail.com>
2017-06-24 19:49:45 -0700

    set word limit per article

--- application/views/articles/index.php
+++ application/views/articles/index.php
@@ -5,7 +5,7 @@
 
     <h3><?php echo $article['title']; ?></h3>
     <small class="post-date">Posted on: <?php echo $article['created_at']; ?></small><br>
-    <?php echo $article['body']; ?>
+    <?php echo word_limiter($article['body'], 50); ?>
     <br><br>
     <p><a class="btn btn-primary" href="<?php echo site_url('/articles/view/'.$article['slug']); ?>">Read More</a></p>
 
