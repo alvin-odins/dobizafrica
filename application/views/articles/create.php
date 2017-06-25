049ee76adf14db5517871f8786097867d65ac8f5
alvin <alvin.odins@gmail.com>
2017-06-24 17:33:28 -0700

    error display | load form validation lib

--- application/views/articles/create.php
+++ application/views/articles/create.php
@@ -1,6 +1,10 @@
 <div class="container">
   <br><br><br>
   <h2><?= $title; ?></h2>
+
+  <?php echo validation_errors(); ?>
+
+
   <?php echo form_open('articles/create') ?>
     <div class="form-group">
       <label>Title:</label>
