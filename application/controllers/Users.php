b02f25bf143a3b0f95a24d19217a3dba714b5c39
alvin <alvin.odins@gmail.com>
2017-06-26 04:31:03 -0700

    create user register form

--- application/controllers/Users.php
+++ application/controllers/Users.php
@@ -4,11 +4,11 @@
     public function register(){
       $data['title'] = 'Register';
 
-      $this->form_validation->set_rules('name', 'Name', 'required');
-      $this->form_validation->set_rules('username', 'Username', 'required');
-      $this->form_validation->set_rules('email', 'Email', 'required');
-      $this->form_validation->set_rules('password', 'Password', 'required');
-      $this->form_validation->set_rules('password2', 'Confirm Password', 'matches[password]');
+      $this->form_validation->set_rules('name', 'Name', 'trim|required|alpha|min_length[4]|max_length[30]');
+      $this->form_validation->set_rules('username', 'Username', 'trim|required|alpha|min_length[4]|max_length[12]');
+      $this->form_validation->set_rules('email', 'Email', 'trim|required|valid_email|is_unique[user.email]');
+      $this->form_validation->set_rules('password', 'Password', 'trim|required');
+      $this->form_validation->set_rules('password2', 'Confirm Password', 'trim|matches[password]');
 
       if ($this->form_validation->run() === FALSE) {
         $this->load->view('templates/header');
--- application/views/users/register.php
+++ application/views/users/register.php
@@ -1,8 +1,35 @@
 <div class="container">
-<h2><?= $title; ?></h2>
+<div class="row">
+	<div class="col-md-8 col-sm-8 col-xs-8 col-md-offset-2 col-sm-offset-2 col-xs-offset-2">
+		<h2><?= $title; ?></h2>
+		<?php echo validation_errors(); ?>
 
-<?php echo validation_errors(); ?>
-
-<?php echo form_open('users/register'); ?>
-	
-<?php echo form_close(); ?>
+		<?php echo form_open('users/register'); ?>
+			<div class="form-group">
+				<label>Name</label>
+				<input type="text" name="name" class="form-control" placeholder="Name">
+			</div>
+			<div class="form-group">
+				<label>Country</label>
+				<input type="text" name="country" class="form-control" placeholder="Country">
+			</div>
+			<div class="form-group">
+				<label>Email</label>
+				<input type="email" name="email" class="form-control" placeholder="Email">
+			</div>
+			<div class="form-group">
+				<label>Username</label>
+				<input type="text" name="username" class="form-control" placeholder="Username">
+			</div>
+			<div class="form-group">
+				<label>Password</label>
+				<input type="password" name="password" class="form-control" placeholder="Password">
+			</div>
+			<div class="form-group">
+				<label>Confirm Password</label>
+				<input type="password" name="password2" class="form-control" placeholder="Confirm Password">
+			</div>
+			<button type="submit" class="btn btn-primary">Sign up</button>
+		<?php echo form_close(); ?>
+			</div>
+		</div>
